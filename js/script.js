74f354df715f61fe46f1aeac1f0b04f0980d4368
RuggeroCoco <ruggerococo94@gmail.com>
2023-03-13 20:06:44 +0100

    Esercizio Mail

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+const searchInput = document.getElementById("search-input");
+const availableEmails = [
+  "user1@example.com",
+  "user2@example.com",
+  "user3@example.com",
+  "user4@example.com",
+  "user5@example.com",
+  "user6@example.com",
+];
+
+const submitBtn = document.getElementById("submit-btn");
+
+const result = document.getElementById("result");
+
+submitBtn.addEventListener("click", function () {
+  console.log(searchInput.value);
+  if (availableEmails.includes(searchInput.value)) {
+    result.innerHTML = "Accesso consentito";
+  } else {
+    result.innerHTML = "Accesso negato";
+  }
+});
