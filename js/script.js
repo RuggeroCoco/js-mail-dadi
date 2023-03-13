40aa15dea0cca0aa005e6ca63b12c6872f5e746b
RuggeroCoco <ruggerococo94@gmail.com>
2023-03-13 21:43:03 +0100

    Esercizio Dadi

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
-
-const searchInput = document.getElementById("search-input");
-const availableEmails = [
-  "user1@example.com",
-  "user2@example.com",
-  "user3@example.com",
-  "user4@example.com",
-  "user5@example.com",
-  "user6@example.com",
-];
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 const submitBtn = document.getElementById("submit-btn");
 
-const result = document.getElementById("result");
+let userNumber = Math.floor(Math.random() * 6) + 1;
+console.log(`Il tuo numero è ${userNumber}`);
+
+let pcNumber = Math.floor(Math.random() * 6) + 1;
+console.log(`Il numero del computer è ${pcNumber}`);
 
 submitBtn.addEventListener("click", function () {
-  console.log(searchInput.value);
-  if (availableEmails.includes(searchInput.value)) {
-    result.innerHTML = "Accesso consentito";
-  } else {
-    result.innerHTML = "Accesso negato";
-  }
+    if (userNumber > pcNumber) {
+        result.innerHTML = "Hai vinto!";
+    } else if (pcNumber > userNumber); {
+        result.innerHTML = "Hai perso!";
+    } else (pcNumber === userNumber); {
+       result.innerHTML = "Avete pareggiato";
+    }
 });
